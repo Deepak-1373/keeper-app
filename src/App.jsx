3a242778778da7dd28d1bbbe9f03b891c2f0be87
Deepak Kamat <dkamat.001@gmail.com>
2022-03-21 21:15:55 +0530

    feat: add navbar component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Navbar } from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <div className="app">
+      {/* Navbar */}
+      <Navbar />
+      {/* Sidebar */}
+      {/* main container */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import bulbImage from "../../assests/bulb.png";
+import profileImage from "../../assests/profile.jpg";
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar flex-row space-between items-center p-8">
+      <div className="navbar-brand flex-row items-center justify-center">
+        <img src={bulbImage} alt="Keeper App" />
+        <p className="brand-text">Keeper</p>
+      </div>
+      <input
+        className="navbar-search font-default"
+        type="text"
+        placeholder="Search"
+      />
+      <img className="navbar-profile" src={profileImage} alt="Profile logo" />
+    </div>
+  );
+};
