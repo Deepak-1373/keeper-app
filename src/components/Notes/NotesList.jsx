b3aa85a637d30d10463b0f6bcd9bea59b2bb9a0e
Deepak Kamat <dkamat.001@gmail.com>
2022-03-22 16:54:07 +0530

    styles: add css for notes list component

--- src/components/Notes/NotesList.jsx
+++ src/components/Notes/NotesList.jsx
@@ -2,5 +2,56 @@ import React from "react";
 import "./NotesList.css";
 
 export const NotesList = () => {
-  return <div className="notes">NotesList</div>;
+  return (
+    <div className="notes-list-container flex-row justify-center">
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+      <div className="note">
+        <h3>Title</h3>
+        <p>Content</p>
+      </div>
+    </div>
+  );
 };
