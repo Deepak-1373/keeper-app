95234cd197bccd08ef7e3628f2eac5d4ef7feb28
Deepak Kamat <dkamat.001@gmail.com>
2022-03-22 13:20:43 +0530

    style: add styles for noteform input field

--- src/components/NotesForm/NotesForm.jsx
+++ src/components/NotesForm/NotesForm.jsx
@@ -3,10 +3,19 @@ import "./NotesForm.css";
 
 export const NotesForm = () => {
   return (
-    <div className="form">
-      <form className="flex-column items-center justify-center">
-        <input type="text" />
-        <textarea name="" id="" cols="30" rows="10"></textarea>
+    <div className="form-container">
+      <form className="form-area flex-column items-center justify-center">
+        <div className="input-container flex-column border-round">
+          <input
+            className="form-input border-round color-white"
+            type="text"
+            placeholder="Title"
+          />
+          <textarea
+            className="text-input-area color-white"
+            placeholder="Take a note"
+          ></textarea>
+        </div>
       </form>
     </div>
   );
