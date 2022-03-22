76e5c087886594fa9df6d7ccbb70c60dade23571
Deepak Kamat <dkamat.001@gmail.com>
2022-03-22 14:53:50 +0530

    styles: add utility classes to input fields

--- src/components/NotesForm/NotesForm.jsx
+++ src/components/NotesForm/NotesForm.jsx
@@ -7,12 +7,12 @@ export const NotesForm = () => {
       <form className="form-area flex-column items-center justify-center">
         <div className="input-container flex-column border-round">
           <input
-            className="form-input border-round color-white"
+            className="form-input border-round color-white disable-border disable-outline"
             type="text"
             placeholder="Title"
           />
           <textarea
-            className="text-input-area color-white"
+            className="text-input-area color-white disable-border disable-outline"
             placeholder="Take a note"
           ></textarea>
         </div>
