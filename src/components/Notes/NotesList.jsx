814672af96247ac0093c6fdac0baf04cddc68d99
Deepak Kamat <dkamat.001@gmail.com>
2022-03-23 12:59:52 +0530

    feat: show notesy

--- src/components/Notes/NotesList.jsx
+++ src/components/Notes/NotesList.jsx
@@ -1,57 +1,19 @@
 import React from "react";
+import { useNotes } from "../../context";
 import "./NotesList.css";
 
 export const NotesList = () => {
+  const { notesList } = useNotes();
+
   return (
     <div className="notes-list-container flex-row justify-center">
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
-      <div className="note">
-        <h3>Title</h3>
-        <p>Content</p>
-      </div>
+      {notesList.length &&
+        notesList.map(({ id, title, content }) => (
+          <div key={id} className="note">
+            <h3>{title}</h3>
+            <p>{content}</p>
+          </div>
+        ))}
     </div>
   );
 };
