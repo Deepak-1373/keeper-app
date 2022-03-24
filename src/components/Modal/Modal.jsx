f1ed84489777328ca56bc97f093311992922c193
Deepak Kamat <dkamat.001@gmail.com>
2022-03-24 20:34:54 +0530

    style: add position to close modal button

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Close from "@material-ui/icons/CloseRounded";
 import { useNotes } from "../../context";
 import "./Modal.css";
 
@@ -10,10 +11,17 @@ export const Modal = () => {
         openModal && "show-modal-bg"
       } modal-background flex justify-center items-center w-screen h-screen fixed inset-0`}
     >
-      <div className="modal-container flex flex-col justify-center items-start px-4 py-3">
+      <div className="modal-container relative flex flex-col justify-center items-start px-4 py-3">
         <h3>Title</h3>
         <p>content</p>
-        <button onClick={() => setOpenModal(false)}>X</button>
+        <div className="close-modal absolute top-0 right-0">
+          <button
+            className="bg-inherit text-white border-base cursor-pointer"
+            onClick={() => setOpenModal(false)}
+          >
+            <Close></Close>
+          </button>
+        </div>
         <div className="w-full flex justify-end items-center">
           <button className="edit-btn px-4 py-3 rounded-lg bg-inherit text-white border-base cursor-pointer">
             Edit
