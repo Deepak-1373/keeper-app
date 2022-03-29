dd322f462603185e2ab5ee3dad4cbcc5f9ef6512
Deepak Kamat <dkamat.001@gmail.com>
2022-03-29 20:34:41 +0530

    style: add active class

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
 import { sidebarNames } from "./data";
 
 export const Sidebar = () => {
+  const [active, setActive] = useState();
+
+  const handleSidebarClick = (listIdx) => {
+    active === listIdx ? setActive(listIdx) : setActive(listIdx);
+  };
+
   return (
     <div className="sidebar">
       <ul className="text-white text-base cursor-pointer">
-        {sidebarNames.map(({ id, sidebarName, iconName }) => (
+        {sidebarNames.map(({ id, sidebarName, iconName }, idx) => (
           <>
-            <li key={id} className="sidebar-item flex items-center">
+            <li
+              onClick={() => handleSidebarClick(idx)}
+              key={id}
+              className={`${
+                active === idx && "active"
+              }  sidebar-item flex items-center`}
+            >
               <span className="sidebar-icon material-icons-outlined">
                 {iconName}
               </span>
