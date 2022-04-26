2692bf2cb0c7defb45762be0ffc950cfe08ab3bf
Deepak Kamat <dkamat.001@gmail.com>
2022-04-26 21:25:09 +0530

    style: add styles for label modal

--- src/components/Label/Label.jsx
+++ src/components/Label/Label.jsx
@@ -22,19 +22,37 @@ export const Label = ({ openLabelModal, setOpenLabelModal }) => {
         } label-modal-bg flex justify-center items-center w-screen h-screen fixed inset-0`}
       >
         <div className="label-modal-content relative flex flex-col justify-center items-start px-4 py-3 rounded-lg border-base text-white">
-          <div>
+          <p>Edit Labels</p>
+          <div className="labels-list">
             {labels.length > 0 &&
-              labels.map((label) => <li key={label}>{label}</li>)}
+              labels.map((label) => (
+                <>
+                  <li key={label}>{label}</li>
+                  <button>Remove</button>
+                </>
+              ))}
           </div>
-          <div>
+          <div className="label-form flex">
             <input
+              type="text"
               onChange={(e) => setLabelInput(e.target.value)}
               value={labelInput}
+              className="label-input"
             />
-            <button onClick={() => handleLabelClick()}>+</button>
-            <button>Remove</button>
+            <button
+              className="add-label-btn rounded-lg text-white bg-inherit border-none cursor-pointer"
+              onClick={() => handleLabelClick()}
+            >
+              <span class="material-symbols-outlined">check</span>
+            </button>
           </div>
-          <button onClick={() => setOpenLabelModal(false)}>x</button>
+          <hr className="w-full" />
+          <button
+            className="modal-close-btn"
+            onClick={() => setOpenLabelModal(false)}
+          >
+            Done
+          </button>
         </div>
       </div>
     </div>
