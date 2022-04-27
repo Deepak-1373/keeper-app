081809d68667143b90ede9b4826d2d57c2f4130c
Deepak Kamat <dkamat.001@gmail.com>
2022-04-27 10:34:13 +0530

    style: add styles to labels list and icons

--- src/components/Label/Label.jsx
+++ src/components/Label/Label.jsx
@@ -22,37 +22,52 @@ export const Label = ({ openLabelModal, setOpenLabelModal }) => {
         } label-modal-bg flex justify-center items-center w-screen h-screen fixed inset-0`}
       >
         <div className="label-modal-content relative flex flex-col justify-center items-start px-4 py-3 rounded-lg border-base text-white">
-          <p>Edit Labels</p>
-          <div className="labels-list">
-            {labels.length > 0 &&
-              labels.map((label) => (
-                <>
-                  <li key={label}>{label}</li>
-                  <button>Remove</button>
-                </>
-              ))}
-          </div>
+          <p className="labels-header">Edit Labels</p>
           <div className="label-form flex">
             <input
               type="text"
               onChange={(e) => setLabelInput(e.target.value)}
               value={labelInput}
-              className="label-input"
+              className="label-input border-none outline-none"
             />
             <button
               className="add-label-btn rounded-lg text-white bg-inherit border-none cursor-pointer"
               onClick={() => handleLabelClick()}
             >
-              <span class="material-symbols-outlined">check</span>
+              <span className="text-base material-symbols-outlined">check</span>
             </button>
           </div>
+
+          {labels.length > 0 &&
+            labels.map((label) => (
+              <div
+                className="labels-list w-full flex items-center justify-between"
+                key={label}
+              >
+                <div className="flex items-center">
+                  <button className="delete-label-btn bg-inherit text-white border-none cursor-pointer">
+                    <span className="text-base material-symbols-outlined">
+                      delete
+                    </span>
+                  </button>
+                  <li className="label-name text-base">{label}</li>
+                </div>
+                <button className="edit-label-btn bg-inherit text-white border-none cursor-pointer">
+                  <span className="text-base material-symbols-outlined">
+                    edit
+                  </span>
+                </button>
+              </div>
+            ))}
           <hr className="w-full" />
-          <button
-            className="modal-close-btn"
-            onClick={() => setOpenLabelModal(false)}
-          >
-            Done
-          </button>
+          <div className="w-full flex justify-end item-center">
+            <button
+              className="modal-close-btn rounded-lg text-white bg-inherit border-none border-base cursor-pointer"
+              onClick={() => setOpenLabelModal(false)}
+            >
+              Done
+            </button>
+          </div>
         </div>
       </div>
     </div>
