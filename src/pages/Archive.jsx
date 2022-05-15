455c60344e4767dc677d8c0d41ecb129de2bd3dd
Deepak Kamat <dkamat.001@gmail.com>
2022-05-15 11:34:28 +0530

    feat(style): add layout for archive page

--- src/pages/Archive.jsx
+++ src/pages/Archive.jsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { Sidebar } from "../components";
+import { useNotes, useTheme } from "../context";
 
 export const Archive = () => {
-  return <div>This is Archive Page</div>;
+  const { archiveList, searchQuery } = useNotes();
+  const { theme } = useTheme();
+  return (
+    <div className="container">
+      <Sidebar />
+      <div className="flex justify-start items-start flex-wrap">
+        {archiveList &&
+          archiveList
+            .filter((note) => {
+              return !searchQuery
+                ? note
+                : note.title.toLowerCase().includes(searchQuery.toLowerCase());
+            })
+            .map(({ id, title, content, backgroundColor }) => (
+              <div
+                style={{ backgroundColor: backgroundColor[theme] }}
+                key={id}
+                className="note rounded-lg relative w-full px-4 py-3 border-base cursor-pointer"
+              >
+                <h3>{title}</h3>
+                <p>{content}</p>
+              </div>
+            ))}
+      </div>
+    </div>
+  );
 };
