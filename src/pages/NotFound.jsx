4158f4a620bfecf1e1240d4953927e93dc6019c8
Deepak Kamat <dkamat.001@gmail.com>
2022-05-16 21:10:41 +0530

    feat: complete not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PageNotFound from "../assests/pagenotfound.png";
 
 export const NotFound = () => {
-  return <div>Sorry, the page you are looking for does not exist</div>;
+  return (
+    <div className="page-not-found-container flex flex-col items-center justify-center">
+      <img src={PageNotFound} alt="Page not found" />
+      <Link to="/">
+        <button className="page-not-found-btn flex items-center justify-center px-4 py-3 rounded-lg bg-inherit text-white border-base cursor-pointer">
+          <span className="page-not-found-icon material-symbols-outlined">
+            arrow_back
+          </span>
+          Go back to notes
+        </button>
+      </Link>
+    </div>
+  );
 };
